54427465d98d43df676690fabd1c5191b38e3b49
Ardhi <ardhi333@gmail.com>
2023-12-10 13:35:09 +0700

    10

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -21,14 +21,14 @@ const createServer = async (container) => {
     },
   ]);
 
-  // server.route({
-  //   method: 'GET',
-  //   path: '/',
-  //   handler: () => ({
-  //     value: 'Hello world!',
-  //   }),
-  // });
-
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hello world!',
+    }),
+  });
+  // oke
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
